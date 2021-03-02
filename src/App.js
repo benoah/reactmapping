27c6a3e3a4974266c0d739658e4a74e57487f4df
ben <noahbenaas@gmail.com>
2021-03-02 17:56:03 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,55 +1,50 @@
-import React, { Component } from "react";
+// https://scrimba.com/g/greacthooks
+// Uncaught Invariant Violation: Maximum update depth exceeded. This can happen when a component repeatedly calls setState inside componentWillUpdate or componentDidUpdate. React limits the number of nested updates to prevent infinite loops.
 
-// https://engineering.musefind.com/react-lifecycle-methods-how-and-when-to-use-them-2111a1b692b1
-// https://reactjs.org/blog/2018/03/29/react-v-16-3.html#component-lifecycle-changes
+import React from "react";
+import randomcolor from "randomcolor";
 
-class TodoList extends Component {
+class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      count: 0,
+      color: "",
+    };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
 
-  static getDerivedStateFromProps(props, state) {
-    // return the new, updated state based upon the props
-    // https://reactjs.org/docs/react-component.html#static-getderivedstatefromprops
-    // https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
+  increment() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  }
+  decrement() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1,
+      };
+    });
   }
 
-  getSnapshotBeforeUpdate() {
-    // create a backup of the current way things are
-    // https://reactjs.org/docs/react-component.html#getsnapshotbeforeupdate
-  }
-
-  // componentWillMount() {
-
-  // }
-
-  componentDidMount() {
-    // GET the data I need to correctly display
-  }
-
-  // componentWillReceiveProps(nextProps) {
-  //     if (nextProps.whatever !== this.props.whatever) {
-  //         // do something important here
-  //     }
-  // }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    // return true if want it to update
-    // return false if not
-  }
-
-  // componentWillUpdate() {
-
-  // }
-
-  componentWillUnmount() {
-    // teardown or cleanup your code before your component disappears
-    // (E.g. remove event listeners)
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      const newColor = randomcolor();
+      this.setState({ color: newColor });
+    }
   }
 
   render() {
-    return <div>Code goes here</div>;
+    return (
+      <div>
+        <h1 style={{ color: this.state.color }}>{this.state.count}</h1>
+        <button onClick={this.increment}>Increment!</button>
+        <button onClick={this.decrement}>Decrement!</button>
+      </div>
+    );
   }
 }
 
