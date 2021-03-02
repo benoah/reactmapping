15758e30ff7e33b627f34fb1307269eeb1ab5409
ben <noahbenaas@gmail.com>
2021-03-02 18:28:30 +0100

    update

--- src/Conditional.js
+++ src/Conditional.js
@@ -5,27 +5,3 @@ function Conditional(props) {
 }
 
 export default Conditional;
-// function Conditional(props) {
-// return (
-//    <div>
-//    <h1>Navbar</h1>
-
-//  {props.isLoading ? (
-//  <h1>Loading...</h1>
-// ) : (
-//<h1>Some cool stuff about conditional rendering</h1>
-//)}
-
-//<h1>Footer</h1>
-//  </div>
-//);
-//}
-
-// export default Conditional;
-
-// condition ? statement if true : statement if false
-
-//if (props.isLoading === true) {
-// return <h1>Loading...</h1>;
-//} else {
-// return <h1>Some cool stuff about conditional rendering</h1>;
