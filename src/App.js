18427386cd45a1d95cee34c61daa324c4795b28a
ben <noahbenaas@gmail.com>
2021-03-02 17:21:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
 import React from "react";
-import Product from "./Product";
-import productsData from "./vschoolProducts";
-import TodoItem from "./TodoItem";
-import todosData from "./todosData";
 
-import Joke from "./Joke";
-import jokesData from "./jokesData";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-function App() {
-  const todoItems = todosData.map((item) => (
-    <TodoItem key={item.id} item={item} />
-  ));
-
-  const productComponents = productsData.map((item) => (
-    <Product key={item.id} product={item} />
-  ));
-
-  const jokeComponents = jokesData.map((joke) => (
-    <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />
-  ));
-
-  return (
-    <div>
-      {todoItems}
-
-      <div className="product">{productComponents}</div>
-      {jokeComponents}
-    </div>
-  );
+  handleClick() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
-
-/*
-Let's practice props and mapping components on our todo list app!
-
-I've created a js file with some todos data in it, which I'm imported into this file. (Normally this data would come from an API call, not a local file). 
-
-Challenge: Using the array map method, render a child component for each todo item in the todosData array and pass the relevant data to it.
-*/
