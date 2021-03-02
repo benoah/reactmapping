fe0e7d25b84141da97fbe3965e93dd42fa31b6b3
ben <noahbenaas@gmail.com>
2021-03-02 17:40:34 +0100

    update some rules to

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 
-class App extends React.Component {
+// https://engineering.musefind.com/react-lifecycle-methods-how-and-when-to-use-them-2111a1b692b1
+// https://reactjs.org/blog/2018/03/29/react-v-16-3.html#component-lifecycle-changes
+
+class TodoList extends Component {
   constructor() {
     super();
-    this.state = {
-      count: 0,
-    };
-    this.handleClick = this.handleClick.bind(this);
+    this.state = {};
   }
 
-  handleClick() {
-    this.setState((prevState) => {
-      return {
-        count: prevState.count + 1,
-      };
-    });
+  static getDerivedStateFromProps(props, state) {
+    // return the new, updated state based upon the props
+    // https://reactjs.org/docs/react-component.html#static-getderivedstatefromprops
+    // https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
   }
+
+  getSnapshotBeforeUpdate() {
+    // create a backup of the current way things are
+    // https://reactjs.org/docs/react-component.html#getsnapshotbeforeupdate
+  }
+
+  // componentWillMount() {
+
+  // }
+
+  componentDidMount() {
+    // GET the data I need to correctly display
+  }
+
+  // componentWillReceiveProps(nextProps) {
+  //     if (nextProps.whatever !== this.props.whatever) {
+  //         // do something important here
+  //     }
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // return true if want it to update
+    // return false if not
+  }
+
+  // componentWillUpdate() {
+
+  // }
+
+  componentWillUnmount() {
+    // teardown or cleanup your code before your component disappears
+    // (E.g. remove event listeners)
+  }
+
   render() {
-    return (
-      <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handleClick}>Change!</button>
-      </div>
-    );
+    return <div>Code goes here</div>;
   }
 }
 
